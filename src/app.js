d750e556e09ce5f34962f2fac0cf718075d348a2
Guilherme <guisinatora@hotmail.com>
2020-11-04 20:25:45 -0300

    POST route created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,12 +10,27 @@ app.use(cors());
 
 const repositories = [];
 
+// function validateProjectId(request, response, next) {
+//   const { id } = request.params;
+
+//   if (!isUuid(id)) {
+//     return response.status(400).json({ error: 'Invalid project ID.' });
+//   }
+
+//   return next();
+// }
+
 app.get("/repositories", (request, response) => {
-  // TODO
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -29,5 +44,6 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
 });
+
 
 module.exports = app;
